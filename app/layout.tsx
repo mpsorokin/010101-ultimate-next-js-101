46d20af28198262892a487abfb50de1e76d4de14
Aa <aa@example.com>
2025-08-25 20:25:24 +0300

    added eslint plugins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import React from "react";
+import { Geist, Geist_Mono as GeistMono } from "next/font/google";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -7,7 +9,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
