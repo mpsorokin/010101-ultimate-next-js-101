926459b286731495fefd87c061779a61e8308d66
Aa <aa@example.com>
2025-08-26 15:18:07 +0300

    added another font

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,13 @@ import "./globals.css";
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
   variable: "--font-inter",
-  weight: "100 200 300 400 500 700 800 900 ",
+  weight: "100 200 300 400 500 700 800 900",
 });
 
 const spaceGrotesk = localFont({
   src: "./fonts/SpaceGroteskVF.ttf",
   variable: "--font-space-grotesk",
-  weight: "100 200 300 400 500 700 800 900 ",
+  weight: "300 400 500 700",
 });
 
 export const metadata: Metadata = {
@@ -27,7 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+      <body
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
