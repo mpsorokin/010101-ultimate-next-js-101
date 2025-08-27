d8b4344c8f1efda14e1a73c50465eb0eb946bbc5
Aa <aa@example.com>
2025-08-27 15:15:51 +0300

    added mobile nav skeleton

--- components/navigation/navbar/MobileNavigation.tsx
+++ components/navigation/navbar/MobileNavigation.tsx
@@ -1,5 +1,40 @@
+import Image from "next/image";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
 const MobileNavigation = () => {
-  return <div>Mob</div>;
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Image
+          src="/icons/hamburger.svg"
+          width={36}
+          height={36}
+          alt="menu"
+          className="invert-colors sm:hidden"
+        />
+      </SheetTrigger>
+      <SheetContent
+        side="left"
+        className="background-light900_dark200 border-none"
+      >
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNavigation;
