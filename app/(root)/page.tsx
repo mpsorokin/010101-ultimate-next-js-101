7c9e8b6693ba10247a186a591aaa70e203a30ce9
Aa <aa@example.com>
2025-08-27 15:05:17 +0300

    cleaned up

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,14 @@
-import { signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
-export default function Home() {
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
   return (
     <div>
       <h2 className="h1-bold">Home page</h2>
-
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </div>
   );
-}
+};
+
+export default Home;
