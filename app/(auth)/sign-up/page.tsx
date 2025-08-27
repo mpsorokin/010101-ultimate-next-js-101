97d70d0056e099c3da326722a89444509e9e116c
Aa <aa@example.com>
2025-08-27 07:24:28 +0300

    added validation for forms

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
 import AuthForm from "@/components/forms/AuthForm";
+import { SignUpSchema } from "@/lib/validations";
 
 const SignUp = () => {
-  return <AuthForm />;
+  return (
+    <AuthForm
+      formType="SIGN_UP"
+      schema={SignUpSchema}
+      defaultValues={{ email: "", password: "" }}
+      onSubmit={(data) => Promise.resolve({ success: true, data })}
+    />
+  );
 };
 
 export default SignUp;
