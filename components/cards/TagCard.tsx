a060d9d622c39584effb12a04bbe54c0a54749ed
Aa <aa@example.com>
2025-08-28 17:36:37 +0300

    added basic link items

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -20,12 +20,16 @@ const TagCard = ({
 }: ITagCardProps) => {
   return (
     <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
-      <Badge className="subtle-medium background-light800_dark300 text-dark400_light500 rounded-md border-none px-4 py-2 uppercase">
+      <Badge className="background-light800_dark300 text-dark400_light500 rounded-md border-none px-4 py-2 uppercase">
         <div className="flex-center space-x-2">
           <i>ICON</i>
           <span>{name}</span>
         </div>
       </Badge>
+
+      {showCount && (
+        <p className="small-medium text-dark500_light700">{questions}</p>
+      )}
     </Link>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getDeviconClassName = (techName: string) => {
+  const normalizedTechName = techName.replace(/[ .]/g, "").toLowerCase();
+
+  const techMap: { [key: string]: string } = {
+    javascript: "devicon-javascript-plain",
+    js: "devicon-javascript-plain",
+  };
+
+  return `${techMap[normalizedTechName]} colored` || "devicon-devicon-plain";
+};
