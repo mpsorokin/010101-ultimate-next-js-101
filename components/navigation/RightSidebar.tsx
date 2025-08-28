9b810e30025bba1b559adde6964e191c45f8804c
Aa <aa@example.com>
2025-08-28 16:53:35 +0300

    added basic link items

---
+++ components/navigation/RightSidebar.tsx
@@ -0,0 +1,11 @@
+const RightSidebar = () => {
+  return (
+    <section className="pt-36 custom-scrollbar background-light900_dark200 light-border sticky right-0">
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top questions</h3>
+      </div>
+    </section>
+  );
+};
+
+export default RightSidebar;
