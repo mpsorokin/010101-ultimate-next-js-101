04a3db7aeeabbac9a16b5927eadaf5e3f81c3956
Aa <aa@example.com>
2025-08-28 15:48:51 +0300

    added basic link items

--- components/navigation/navbar/LeftSidebar.tsx
+++ components/navigation/navbar/LeftSidebar.tsx
@@ -3,8 +3,8 @@ import NavLinks from "@/components/navigation/navbar/NavLinks";
 const LeftSidebar = () => {
   return (
     <section
-      className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0
-    h-screen flex flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300"
+      className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col
+      justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 darl:shadow-none max-sm:hidden lg:w-[266px]"
     >
       <div className="flex flex-1 flex-col gap-6">
         <NavLinks />
