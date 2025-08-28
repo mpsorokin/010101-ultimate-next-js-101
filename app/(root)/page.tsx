d28197de6aaf4c0f21b680ee65b2cc07a61367ee
Aa <aa@example.com>
2025-08-28 19:22:26 +0300

    added basic link items

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,21 @@
-import { auth } from "@/auth";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
-  const session = await auth();
-  console.log(session);
-
   return (
     <div>
-      <h2 className="h1-bold">Home page</h2>
+      <section className="w-full flex flex-col-reverse sm:flex-row justify-between gap-4 sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a question</Link>
+        </Button>
+      </section>
     </div>
   );
 };
