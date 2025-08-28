c69622ffa8acf7730891f4ce97bdf204c9b3d98d
Aa <aa@example.com>
2025-08-28 17:27:48 +0300

    added basic link items

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,3 +1,8 @@
+import Link from "next/link";
+
+import { Badge } from "@/components/ui/badge";
+import ROUTES from "@/constants/routes";
+
 interface ITagCardProps {
   _id: string;
   name: string;
@@ -13,7 +18,16 @@ const TagCard = ({
   showCount,
   compact,
 }: ITagCardProps) => {
-  return <p>Tag</p>;
+  return (
+    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+      <Badge className="subtle-medium background-light800_dark300 text-dark400_light500 rounded-md border-none px-4 py-2 uppercase">
+        <div className="flex-center space-x-2">
+          <i>ICON</i>
+          <span>{name}</span>
+        </div>
+      </Badge>
+    </Link>
+  );
 };
 
 export default TagCard;
