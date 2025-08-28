6cc0dd9c4861b039d66580bef4e1e784665edb60
Aa <aa@example.com>
2025-08-28 15:44:28 +0300

    added basic link items

--- components/navigation/navbar/LeftSidebar.tsx
+++ components/navigation/navbar/LeftSidebar.tsx
@@ -1,5 +1,13 @@
+import NavLinks from "@/components/navigation/navbar/NavLinks";
+
 const LeftSidebar = () => {
-  return <section>Left sidebar</section>;
+  return (
+    <section className="custom-scrollbar">
+      <div className="flex flex-1 flex-col gap-6">
+        <NavLinks />
+      </div>
+    </section>
+  );
 };
 
 export default LeftSidebar;
