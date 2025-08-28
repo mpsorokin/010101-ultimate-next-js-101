e7c6114708c64c3dcb588e8a24f4c87719f664c4
Aa <aa@example.com>
2025-08-28 15:56:37 +0300

    added basic link items

--- components/navigation/navbar/LeftSidebar.tsx
+++ components/navigation/navbar/LeftSidebar.tsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import NavLinks from "@/components/navigation/navbar/NavLinks";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const LeftSidebar = () => {
   return (
@@ -8,6 +13,41 @@ const LeftSidebar = () => {
     >
       <div className="flex flex-1 flex-col gap-6">
         <NavLinks />
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <Button
+          className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
+          asChild
+        >
+          <Link href={ROUTES.SIGN_IN}>
+            <Image
+              src="/icons/account.svg"
+              alt="account"
+              width={20}
+              height={20}
+              className="invert-colors lg:hidden"
+            />
+            <span className="primary-text-gradient max-lg:hidden">Log In</span>
+          </Link>
+        </Button>
+
+        <Button
+          className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full
+         rounded-lg border px-4 py-3 shadow-none"
+          asChild
+        >
+          <Link href={ROUTES.SIGN_UP}>
+            <Image
+              src="/icons/sign-up.svg"
+              alt="account"
+              width={20}
+              height={20}
+              className="invert-colors lg:hidden"
+            />
+            <span className="max-lg:hidden">Sign Up</span>
+          </Link>
+        </Button>
       </div>
     </section>
   );
