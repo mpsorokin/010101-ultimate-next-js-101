35adaa14bab3e45ab190e9ec99bab2a1bc6b2cbe
Aa <aa@example.com>
2025-08-28 17:38:15 +0300

    added basic link items

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 import { Badge } from "@/components/ui/badge";
 import ROUTES from "@/constants/routes";
+import { getDeviconClassName } from "@/lib/utils";
 
 interface ITagCardProps {
   _id: string;
@@ -18,11 +19,13 @@ const TagCard = ({
   showCount,
   compact,
 }: ITagCardProps) => {
+  const iconClass = getDeviconClassName(name);
+
   return (
     <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
       <Badge className="background-light800_dark300 text-dark400_light500 rounded-md border-none px-4 py-2 uppercase">
         <div className="flex-center space-x-2">
-          <i>ICON</i>
+          <i className={iconClass}></i>
           <span>{name}</span>
         </div>
       </Badge>
