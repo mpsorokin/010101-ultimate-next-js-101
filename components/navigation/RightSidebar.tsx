066ce3890e48f0b5acbc06847aaa822a56dac382
Aa <aa@example.com>
2025-08-28 17:07:14 +0300

    added basic link items

--- components/navigation/RightSidebar.tsx
+++ components/navigation/RightSidebar.tsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import ROUTES from "@/constants/routes";
+
 const hotQuestions = [
   { _id: "1", title: "How to create a custom hook in React?" },
   { _id: "2", title: "How to use React Query?" },
   { _id: "3", title: "How to use Redux?" },
   { _id: "4", title: "How to use React Router?" },
+  { _id: "5", title: "How to use React Context?" },
 ];
 
 const RightSidebar = () => {
@@ -14,7 +20,23 @@ const RightSidebar = () => {
       <div>
         <h3 className="h3-bold text-dark200_light900">Top questions</h3>
 
-        <div className="mt-7 flex w-full flex-col gap-[30px]"></div>
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {hotQuestions.map(({ _id, title }) => (
+            <Link
+              href={ROUTES.PROFILE(_id)}
+              key={_id}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
+              <p className="body-medium text-dark500_light700">{title}</p>
+              <Image
+                src="/icons/chevron-right.svg"
+                alt="chevron"
+                width={20}
+                height={20}
+              />
+            </Link>
+          ))}
+        </div>
       </div>
     </section>
   );
