c180a90d8de4182d5a129cebd0864d0e1239d790
Aa <aa@example.com>
2025-08-29 14:49:57 +0300

    added home filter

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -10,6 +10,7 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
+  FormDescription,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { AskQuestionSchema } from "@/lib/validations";
@@ -46,9 +47,62 @@ const QuestionForm = () => {
                   type={"text"}
                   {...field}
                   className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700
-                   no-focus min-h-[56px] rounded-1.5 border"
+                   no-focus min-h-[56px] border"
                 />
               </FormControl>
+              <FormDescription className="body-regular text-light-500 mt-2.5">
+                Be specific
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="capitalize paragraph-semibold text-dark400_light800">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  required
+                  type={"text"}
+                  {...field}
+                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700
+                   no-focus min-h-[56px] border"
+                />
+              </FormControl>
+              <FormDescription className="body-regular text-light-500 mt-2.5">
+                Be specific
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="capitalize paragraph-semibold text-dark400_light800">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  required
+                  type={"text"}
+                  {...field}
+                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700
+                   no-focus min-h-[56px] border"
+                />
+              </FormControl>
+              <FormDescription className="body-regular text-light-500 mt-2.5">
+                Be specific
+              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
