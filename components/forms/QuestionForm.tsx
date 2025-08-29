98bb33fefa78c6e22c40586b45b1fec0fa140371
Aa <aa@example.com>
2025-08-29 14:41:03 +0300

    added home filter

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -1,4 +1,24 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Form } from "@/components/ui/form";
+import { AskQuestionSchema } from "@/lib/validations";
+
 const QuestionForm = () => {
-  return <p>qf</p>;
+  const form = useForm({
+    resolver: zodResolver(AskQuestionSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      tags: [],
+    },
+  });
+  return (
+    <Form {...form}>
+      <form className="flex w-full flex-col gap-10"></form>
+    </Form>
+  );
 };
 export default QuestionForm;
