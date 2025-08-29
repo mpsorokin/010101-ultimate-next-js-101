03eba7f4a180fa51380612dd448ae6e42be12889
Aa <aa@example.com>
2025-08-29 11:21:50 +0300

    added home filter

--- components/filters/HomeFilter.tsx
+++ components/filters/HomeFilter.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 const filters = [
   { name: "Newest", value: "newest" },
@@ -8,10 +14,24 @@ const filters = [
 ];
 
 const HomeFilter = () => {
+  const searchParams = useSearchParams();
+  const filterParams = searchParams.get("filter");
+  const [active, setActive] = useState<string>(filterParams || "");
+
   return (
-    <div className="mt-10">
+    <div className="mt-10 hidden flex-wrap gap-3 sm:flex">
       {filters.map((filter) => (
-        <Button key={filter.name}>{filter.name}</Button>
+        <Button
+          key={filter.name}
+          className={cn(
+            `body-medium rounded-lg px-6 py-3 capitalize shadow-none`,
+            active === filter.value
+              ? "bg-primary-100 text-primary-500"
+              : "bg-light-800 text-light-500 hover:bg-light-800 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-300",
+          )}
+        >
+          {filter.name}
+        </Button>
       ))}
     </div>
   );
