9b743ccab137fec9465ced68b2c56d87ba8da92a
Aa <aa@example.com>
2025-08-29 12:24:19 +0300

    added home filter

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,3 +1,4 @@
+import { getTimeStamp } from "@/lib/utils";
 import { IQuestion } from "@/types/global";
 
 interface IQuestionCardProps {
@@ -8,10 +9,10 @@ const QuestionCard = ({
   question: { _id, title, tags, author, createdAt, upvotes, answers, views },
 }: IQuestionCardProps) => {
   return (
-    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+    <div key={_id} className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
-          <span>{createdAt.toString()}</span>
+          <span>{getTimeStamp(createdAt)}</span>
         </div>
       </div>
     </div>
