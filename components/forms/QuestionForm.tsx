1df0dc09ca7bd82c9582b2d07b91ac0305213700
Aa <aa@example.com>
2025-08-29 14:47:40 +0300

    added home filter

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -3,7 +3,15 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
-import { Form } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { AskQuestionSchema } from "@/lib/validations";
 
 const QuestionForm = () => {
@@ -15,9 +23,37 @@ const QuestionForm = () => {
       tags: [],
     },
   });
+
+  const handleCreateQuestion = () => {};
+
   return (
     <Form {...form}>
-      <form className="flex w-full flex-col gap-10"></form>
+      <form
+        className="flex w-full flex-col gap-10"
+        onSubmit={form.handleSubmit(handleCreateQuestion)}
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="capitalize paragraph-semibold text-dark400_light800">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  required
+                  type={"text"}
+                  {...field}
+                  className="paragraph-regular background-light700_dark300 light-border-2 text-dark300_light700
+                   no-focus min-h-[56px] rounded-1.5 border"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </form>
     </Form>
   );
 };
