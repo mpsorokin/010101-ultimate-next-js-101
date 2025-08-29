fe59576a71843423d8e1485b4ce94beaf3624ac7
Aa <aa@example.com>
2025-08-29 12:28:24 +0300

    added home filter

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+
+import ROUTES from "@/constants/routes";
 import { getTimeStamp } from "@/lib/utils";
 import { IQuestion } from "@/types/global";
 
@@ -13,6 +16,12 @@ const QuestionCard = ({
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span>{getTimeStamp(createdAt)}</span>
+
+          <Link href={ROUTES.QUESTION(_id)}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {title}
+            </h3>
+          </Link>
         </div>
       </div>
     </div>
