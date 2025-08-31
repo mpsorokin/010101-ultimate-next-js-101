c44b441aa70dc5d34586d21cd8ac97fee83b2f7c
Aa <aa@example.com>
2025-08-31 10:24:18 +0300

    added logger

--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { ZodError } from "zod";
 
 import { RequestError, ValidationError } from "@/lib/http-errors";
+import logger from "@/lib/logger";
 
 export type ResponseType = "api" | "server";
 
@@ -26,6 +27,11 @@ const formatResponse = (
 
 const handleError = (error: unknown, responseType: ResponseType = "server") => {
   if (error instanceof RequestError) {
+    logger.error(
+      { err: error },
+      `${responseType.toUpperCase()} Error: ${error.message}`,
+    );
+
     return formatResponse(
       responseType,
       error.statusCode,
