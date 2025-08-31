f8a51b71ba386f9931422f16cbb969aeaf558597
Aa <aa@example.com>
2025-08-31 11:36:59 +0300

    added api users route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 
 import User from "@/database/user.model";
 import handleError from "@/lib/handlers/error";
+import { ValidationError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
+import { UserSchema } from "@/lib/validations";
 import { APIErrorResponse } from "@/types/global";
 
 export async function GET() {
@@ -10,6 +12,38 @@ export async function GET() {
     await dbConnect();
     const users = await User.find();
     return NextResponse.json({ success: true, data: users }, { status: 200 });
+  } catch (e) {
+    return handleError(e, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+
+    const validatedData = UserSchema.safeParse(body);
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const { email, username } = validatedData.data;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
+
+    const existingUserName = await User.findOne({ username });
+
+    if (existingUserName) {
+      throw new Error("UserName already exists");
+    }
+
+    const newUser = await User.create(validatedData.data);
+
+    return NextResponse.json({ success: true, data: newUser }, { status: 201 });
   } catch (e) {
     return handleError(e, "api") as APIErrorResponse;
   }
--- database/user.model.ts
+++ database/user.model.ts
@@ -14,7 +14,7 @@ export interface IUser {
 const UserSchema = new Schema(
   {
     name: { type: String, required: true },
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     bio: { type: String },
     image: { type: String },
