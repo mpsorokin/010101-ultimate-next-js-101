4cb7dd29198c0b2ccc4f4c1be7240ed55fb57136
Aa <aa@example.com>
2025-09-01 16:10:14 +0300

    added missing parts from oauth

--- database/account.model.ts
+++ database/account.model.ts
@@ -1,22 +1,23 @@
-import { model, models, Schema, Types } from "mongoose";
+import { model, models, Schema, Types, Document } from "mongoose";
 
 export interface IAccount {
   userId: Types.ObjectId;
   name: string;
+  image?: string;
   password?: string;
   provider: string;
-  providerAccountId?: string;
-  image?: string;
+  providerAccountId: string;
 }
 
+export interface IAccountDoc extends IAccount, Document {}
 const AccountSchema = new Schema<IAccount>(
   {
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     name: { type: String, required: true },
+    image: { type: String },
     password: { type: String },
     provider: { type: String, required: true },
-    providerAccountId: { type: String },
-    image: { type: String },
+    providerAccountId: { type: String, required: true },
   },
   { timestamps: true },
 );
