956c75424ba26c04ef01d219c13eb036faa4f9f8
Aa <aa@example.com>
2025-09-01 11:51:34 +0300

    updated api with account

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,29 @@
+import { ValidationError } from "ajv";
+import { NextResponse } from "next/server";
+
+import Account from "@/database/account.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError } from "@/lib/http-errors";
+import { AccountSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json();
+
+  try {
+    const validatedData = AccountSchema.partial().safeParse({
+      providerAccountId,
+    });
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const account = await Account.findOne({ providerAccountId });
+
+    if (!account) throw new NotFoundError("User does not exist");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (e) {
+    return handleError(e, "api") as APIErrorResponse;
+  }
+}
