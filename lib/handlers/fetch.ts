59ed31e0a7416c7d1ec94a8e7e37776c4147e241
Aa <aa@example.com>
2025-09-01 12:07:04 +0300

    added basic fetch handler

--- lib/handlers/fetch.ts
+++ lib/handlers/fetch.ts
@@ -1,7 +1,12 @@
+import logger from "@/lib/logger";
 import { ActionResponse } from "@/types/global";
 
 interface IFetchOptions extends RequestInit {
   timeout?: number;
+}
+
+function isError(error: unknown): error is Error {
+  return error instanceof Error;
 }
 
 export async function fetchHandler<T>(
@@ -13,4 +18,29 @@ export async function fetchHandler<T>(
     headers: customHeaders = {},
     ...restOptions
   } = options;
+
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+  const defaultHeaders: HeadersInit = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  };
+
+  const headers: HeadersInit = { ...defaultHeaders, ...customHeaders };
+
+  const config: RequestInit = {
+    ...restOptions,
+    headers,
+    signal: controller.signal,
+  };
+
+  try {
+  } catch (e) {
+    const error = isError(e) ? e : new Error("Unknown error");
+
+    if (error.name === "AbortError") {
+      logger.warn(`Request to ${url} timed out`);
+    }
+  }
 }
