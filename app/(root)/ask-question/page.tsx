7180056e99b973d3b6e055d7c8206b145f4afaf9
Aa <aa@example.com>
2025-09-02 16:56:33 +0300

    cleaned up

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,6 +1,15 @@
-import QuestionForm from "@/components/forms/QuestionForm";
+import { redirect } from "next/navigation";
 
-const AskQuestionPage = () => {
+import { auth } from "@/auth";
+import QuestionForm from "@/components/forms/QuestionForm";
+import ROUTES from "@/constants/routes";
+
+const AskQuestionPage = async () => {
+  const session = await auth();
+  if (!session) {
+    return redirect(ROUTES.SIGN_IN);
+  }
+
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
