33f05164d1075f950ad6612e42c106bb66c27459
Aa <aa@example.com>
2025-09-02 17:01:28 +0300

    cleaned up

---
+++ app/(root)/question/[id]/page.tsx
@@ -0,0 +1,7 @@
+import { RouteParams } from "@/types/global";
+
+const QuestionDetailsPage = ({ params }: RouteParams) => {
+  return <p>QuestionDetailsPage</p>;
+};
+
+export default QuestionDetailsPage;
