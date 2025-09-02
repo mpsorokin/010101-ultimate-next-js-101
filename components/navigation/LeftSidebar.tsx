cee62eeb0145f1283924cc57ca71d4ab886bdc0a
Aa <aa@example.com>
2025-09-02 14:17:24 +0300

    added signin credentials handler

--- components/navigation/LeftSidebar.tsx
+++ components/navigation/LeftSidebar.tsx
@@ -1,11 +1,16 @@
+import { LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
+import { auth } from "@/auth";
 import NavLinks from "@/components/navigation/navbar/NavLinks";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
 
-const LeftSidebar = () => {
+const LeftSidebar = async () => {
+  const session = await auth();
+  // console.log(session);
+  const userId = session?.user?.id;
   return (
     <section
       className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 h-screen flex flex-col
@@ -16,38 +21,56 @@ const LeftSidebar = () => {
       </div>
 
       <div className="flex flex-col gap-3">
-        <Button
-          className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
-          asChild
-        >
-          <Link href={ROUTES.SIGN_IN}>
-            <Image
-              src="/icons/account.svg"
-              alt="account"
-              width={20}
-              height={20}
-              className="invert-colors lg:hidden"
-            />
-            <span className="primary-text-gradient max-lg:hidden">Log In</span>
-          </Link>
-        </Button>
+        {userId ? (
+          <form className="mb-10">
+            <Button
+              className="base-medium w-fit !bg-transparent px-4 py-3 cursor-pointer"
+              type="submit"
+            >
+              <LogOut className="size-5 text-black dark:text-white" />
+              <span className="max-lg:hidden text-dark300_light900">
+                Logout
+              </span>
+            </Button>
+          </form>
+        ) : (
+          <>
+            <Button
+              className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
+              asChild
+            >
+              <Link href={ROUTES.SIGN_IN}>
+                <Image
+                  src="/icons/account.svg"
+                  alt="account"
+                  width={20}
+                  height={20}
+                  className="invert-colors lg:hidden"
+                />
+                <span className="primary-text-gradient max-lg:hidden">
+                  Log In
+                </span>
+              </Link>
+            </Button>
 
-        <Button
-          className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full
+            <Button
+              className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full
          rounded-lg border px-4 py-3 shadow-none"
-          asChild
-        >
-          <Link href={ROUTES.SIGN_UP}>
-            <Image
-              src="/icons/sign-up.svg"
-              alt="account"
-              width={20}
-              height={20}
-              className="invert-colors lg:hidden"
-            />
-            <span className="max-lg:hidden">Sign Up</span>
-          </Link>
-        </Button>
+              asChild
+            >
+              <Link href={ROUTES.SIGN_UP}>
+                <Image
+                  src="/icons/sign-up.svg"
+                  alt="account"
+                  width={20}
+                  height={20}
+                  className="invert-colors lg:hidden"
+                />
+                <span className="max-lg:hidden">Sign Up</span>
+              </Link>
+            </Button>
+          </>
+        )}
       </div>
     </section>
   );
