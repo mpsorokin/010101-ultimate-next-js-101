ab33d632887a500d83c2511028559f49c2974b9a
Aa <aa@example.com>
2025-09-03 16:43:40 +0300

    added edit question form

--- components/DataRenderer.tsx
+++ components/DataRenderer.tsx
@@ -1,4 +1,30 @@
-const DataRenderer = () => {
+import { DEFAULT_EMPTY } from "@/constants/states";
+
+interface IDataRendererProps<T> {
+  success: boolean;
+  error?: {
+    message: string;
+    details?: Record<string, string[]>;
+  };
+  data?: T[] | null | undefined;
+  empty?: {
+    title: string;
+    message: string;
+    button?: {
+      text: string;
+      href: string;
+    };
+  };
+  render?: (data: T[]) => React.ReactNode;
+}
+
+const DataRenderer = <T,>({
+  success,
+  error,
+  data,
+  empty = DEFAULT_EMPTY,
+  render,
+}: IDataRendererProps<T>) => {
   return <p>DR</p>;
 };
 
