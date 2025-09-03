1bf13bfaef417d13e87805fb0feb0e4a73d433c9
Aa <aa@example.com>
2025-09-03 14:35:21 +0300

    added edit question form

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -13,8 +13,6 @@ interface IQuestionCardProps {
 const QuestionCard = ({
   question: { _id, title, tags, author, createdAt, upvotes, answers, views },
 }: IQuestionCardProps) => {
-  console.log("author");
-  console.log(author);
   return (
     <div key={_id} className="card-wrapper rounded-[10px] p-9 sm:px-11">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
