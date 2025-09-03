c4a5555e4e594089220e28d7784c9c7d18cff0c4
Aa <aa@example.com>
2025-09-03 12:11:06 +0300

    small type fix

--- app/(root)/question/[id]/edit/page.tsx
+++ app/(root)/question/[id]/edit/page.tsx
@@ -1,20 +1,32 @@
-import { redirect } from "next/navigation";
+import { redirect, notFound } from "next/navigation";
 
 import { auth } from "@/auth";
 import QuestionForm from "@/components/forms/QuestionForm";
 import ROUTES from "@/constants/routes";
+import { getQuestion } from "@/lib/actions/question.action";
 import { RouteParams } from "@/types/global";
 
 const EditQuestionPage = async ({ params }: RouteParams) => {
   const { id } = await params;
 
-  if (!id) throw new Error("id not found");
+  if (!id) {
+    return notFound();
+  }
 
   const session = await auth();
   if (!session) {
     return redirect(ROUTES.SIGN_IN);
   }
 
+  const { data: question, success } = await getQuestion({ questionId: id });
+
+  if (!success) {
+    return notFound();
+  }
+
+  if (question?.author.toString() !== session?.user?.id) {
+    redirect(ROUTES.QUESTION(id));
+  }
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
