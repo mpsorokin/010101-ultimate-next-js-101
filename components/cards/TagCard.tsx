6fac48bbfb0771026c277b4e5cea77e98dc179d4
Aa <aa@example.com>
2025-09-16 18:18:19 +0300

    added tag action

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
 
-import { Badge } from "@/components/ui/badge";
 import ROUTES from "@/constants/routes";
-import { getDeviconClassName, getTechDescription } from "@/lib/utils";
+import { cn, getDeviconClassName, getTechDescription } from "@/lib/utils";
 
-interface ITagCardProps {
+import { Badge } from "../ui/badge";
+
+interface Props {
   _id: string;
   name: string;
   questions?: number;
@@ -25,17 +27,22 @@ const TagCard = ({
   remove,
   isButton,
   handleRemove,
-}: ITagCardProps) => {
+}: Props) => {
   const iconClass = getDeviconClassName(name);
   const iconDescription = getTechDescription(name);
 
+  const handleClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+  };
+
   const Content = (
     <>
-      <Badge className="background-light800_dark300 text-dark400_light500 rounded-md border-none px-4 py-2 uppercase flex flex-row gap-2">
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 flex flex-row gap-2 rounded-md border-none px-4 py-2 uppercase">
         <div className="flex-center space-x-2">
           <i className={`${iconClass} text-sm`}></i>
           <span>{name}</span>
         </div>
+
         {remove && (
           <Image
             src="/icons/close.svg"
@@ -47,6 +54,7 @@ const TagCard = ({
           />
         )}
       </Badge>
+
       {showCount && (
         <p className="small-medium text-dark500_light700">{questions}</p>
       )}
@@ -55,7 +63,7 @@ const TagCard = ({
 
   if (compact) {
     return isButton ? (
-      <button type="button" className="flex justify-between gap-2">
+      <button onClick={handleClick} className="flex justify-between gap-2">
         {Content}
       </button>
     ) : (
@@ -68,7 +76,23 @@ const TagCard = ({
   return (
     <Link href={ROUTES.TAG(_id)} className="shadow-light100_darknone">
       <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-8 py-10 sm:w-[260px]">
-        <div></div>
+        <div className="flex items-center justify-between gap-3">
+          <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
+            <p className="paragraph-semibold text-dark300_light900">{name}</p>
+          </div>
+          <i className={cn(iconClass, "text-2xl")} aria-hidden="true" />
+        </div>
+
+        <p className="small-regular text-dark500_light700 mt-5 line-clamp-3 w-full">
+          {iconDescription}
+        </p>
+
+        <p className="small-medium text-dark400_light500 mt-3.5">
+          <span className="body-semibold primary-text-gradient mr-2.5">
+            {questions}+
+          </span>
+          Questions
+        </p>
       </article>
     </Link>
   );
